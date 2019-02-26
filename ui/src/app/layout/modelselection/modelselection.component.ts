0b2d748cc334e08ec3706e818695a3648dbc3b78
Vincent Fazio <vjf@users.noreply.github.com>
2019-02-27 09:14:36 +1100

    AUS-3195 Removed unused definitions

--- ui/src/app/layout/modelselection/modelselection.component.ts
+++ ui/src/app/layout/modelselection/modelselection.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { routerTransition } from '../../router.animations';
-import { HttpErrorResponse } from '@angular/common/http';
 
 
 
