d1ae741d5d5263352f5f205e1aa786813ff148a3
Vincent Fazio <vjf@users.noreply.github.com>
2020-10-30 12:14:34 +1100

    Add server for api interface

--- ui/docker/build.sh
+++ ui/docker/build.sh
@@ -49,6 +49,7 @@ popd
 pushd ..
 # Build docker image
 sudo docker system prune -af && sudo docker build -t geomodels -f docker/Dockerfile .
+#sudo docker build -t geomodels -f docker/Dockerfile .
 popd
 
 # To run locally at port 4000:
---
+++ ui/docker/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script to start up Apache and Python uvicorn server
+#
+# Apache serves the main website static files and uvicorn serves up the api interface
+
+# Start up uvicorn
+pushd /var/www/html/api && bash -c "uvicorn webapi:app &" && popd
+
+# Start up apache
+/usr/sbin/apache2ctl -D FOREGROUND
