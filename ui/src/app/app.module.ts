5258f81f69997fc9329da5d988fcc2a8a04f9bfe
Vincent Fazio <vjf@users.noreply.github.com>
2025-07-18 13:00:16 +0800

    First step in migrating to Angular 18.2

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
@@ -21,12 +21,10 @@ export function createTranslateLoader(http: HttpClient) {
     return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-    imports: [
-        CommonModule,
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [CommonModule,
         BrowserModule,
         BrowserAnimationsModule,
-        HttpClientModule,
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
@@ -35,22 +33,18 @@ export function createTranslateLoader(http: HttpClient) {
             }
         }),
         AppRoutingModule,
-	NgbModule
-    ],
-    providers: [
+        NgbModule], providers: [
         ModelInfoService,
         SidebarService,
         HelpinfoService,
         // Used when the website is installed in a subdirectory of web server's 'document root'
         // It lets the Angular router know that the base directory of website is a subdirectory of 'document root'
-        {provide: APP_BASE_HREF, useFactory: () => {
-            if (environment.usePrePath) {
-                return environment.prePath;
-            }
-            return '';
-        }}
-    ],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
-})
+        { provide: APP_BASE_HREF, useFactory: () => {
+                if (environment.usePrePath) {
+                    return environment.prePath;
+                }
+                return '';
+            } },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
