512b1d598c2627a2ab6b2ccd83bf4de9d9ef2dc2
Vincent Fazio <vjf@users.noreply.github.com>
2025-07-24 11:37:48 +1000

    Fix type warning

--- ui/src/app/layout/components/header/header.component.ts
+++ ui/src/app/layout/components/header/header.component.ts
@@ -24,8 +24,9 @@ export class HeaderComponent implements OnInit {
         this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
         this.translate.setDefaultLang('en');
         const browserLang = this.translate.getBrowserLang();
-        this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/) ? browserLang : 'en');
-
+        if (browserLang) {
+          this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/) ? browserLang : 'en');
+        }
         this.router.events.subscribe(val => {
             if (
                 val instanceof NavigationEnd &&
