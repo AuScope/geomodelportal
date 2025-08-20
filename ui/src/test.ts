c1614befb49cd2d3ed8d3acffeb50215c3c3b36e
Vincent Fazio <vjf@users.noreply.github.com>
2025-08-20 12:59:40 +1000

    AUS-4233 Update test code

--- ui/src/test.ts
+++ ui/src/test.ts
@@ -8,20 +8,21 @@ import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
+  BrowserTestingModule,
+  platformBrowserTesting
+} from '@angular/platform-browser/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare const __karma__: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = function () { // empty 
+};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
+  BrowserTestingModule,
+  platformBrowserTesting(), {
     teardown: { destroyAfterEach: false }
 }
 );
